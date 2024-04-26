b0ce66a3d8b093a3bc3f9cadda5a63571cadf432
BertGalle <bert.galle@student.howest.be>
2024-04-26 13:59:39 +0200

    removed CNV_ExomeDepth_call.R trailing whitespaces

--- bin/CNV_ExomeDepth_call.R
+++ bin/CNV_ExomeDepth_call.R
@@ -53,7 +53,7 @@ ExomeCount.dafr <- read.table(file=countfile,sep="\t",header = TRUE)
 
 ### Call CNVs ###
 ## build the most appropiate reference set: aggregated reference set
-## key idea: build optimized set of exomens that are well correlated with that exome 
+## key idea: build optimized set of exomens that are well correlated with that exome
 
 ## prepare the main matrix of read count data
 countmat = as.matrix(ExomeCount.dafr[,5:dim(ExomeCount.dafr)[2]]) # remove cols 1-4 metadata
@@ -68,21 +68,21 @@ if (is.element(sampleName, colnames(countmat))){
 }
 
 for (i in 1:nsamples) {
-    
+
     ## select sample-id
     sample = colnames(countmat)[i]
-    
+
     # print(paste(sample,i,families[sample_index]))
     # print(sampleNames[families != families[sample_index]])
 
     ## only CNV calling for input test sample
     if (sample == sampleName) {
-        
+
         sample_index <- which(sampleNames == sample)
 
         ## perform CNV calling for the test sample
         cat("\n*** CNV calling for",sample,"***\n")
-        
+
         ## build reference set (pool = all other samples of run)
         cat("\nSelecting reference samples from pool:\n")
         print(unlist(sampleNames[families != families[sample_index]]))
@@ -93,42 +93,42 @@ for (i in 1:nsamples) {
             bin.length=(ExomeCount.dafr$end-ExomeCount.dafr$start)/1000,
             n.bins.reduced = 10000)
         cat("\nOptimized reference set: ", reference_list[[1]], "\n")
-        
+
         ## construct the reference set (all other samples of run)
         reference_set = apply(
             X = as.matrix(ExomeCount.dafr[, reference_list$reference.choice, drop = FALSE]), #drop = FALSE: in case the reference set contains a singlle sample, it will makes sure that the subsetted object is a data frame, not a numeric vector
             MAR=1, FUN=sum)
-        
+
         ## CNV calling - beta-binomial model applied to the full set of exons - longest step
         cat("\nCreating the ExomeDepth object\n")
-        all_exons = new('ExomeDepth', 
-                        test=countmat[,i], 
+        all_exons = new('ExomeDepth',
+                        test=countmat[,i],
                         reference=reference_set,
                         formula = 'cbind(test,reference) ~ 1')
         ## call CNV by running the underlying hidden Markov model
         ## the correlation between reference and test set should be >0.97
         ## if not: consider the output as less reliable
         cat("\nCall CNVs\n")
-        all_exons = CallCNVs(x = all_exons, 
+        all_exons = CallCNVs(x = all_exons,
                             transition.probability=10^-4,
-                            chromosome=ExomeCount.dafr$chromosome, 
+                            chromosome=ExomeCount.dafr$chromosome,
                             start=ExomeCount.dafr$start,
-                            end=ExomeCount.dafr$end, 
+                            end=ExomeCount.dafr$end,
                             name=ExomeCount.dafr$exon)
         if (all(dim(all_exons@CNV.calls) != c(0,0))) {#if CNVs found
             ## annotation CNV calls with exon info
             cat("\nAnnotate with exon info\n")
-            all_exons = AnnotateExtra(x = all_exons, 
+            all_exons = AnnotateExtra(x = all_exons,
                                 reference.annotation = exons.GRanges,
                                 min.overlap = 0.0001,
                                 column.name= 'exons')
             ## output file
             ## ranking the CNV calls by confidence level
-            ## the BF column = Bayes factor = quantifies the statistical support for each CNV (=log10 of the likelyhood ratio of data for the CNV call divided by the null (normal copy number). 
+            ## the BF column = Bayes factor = quantifies the statistical support for each CNV (=log10 of the likelyhood ratio of data for the CNV call divided by the null (normal copy number).
             ## The higher BF, the more confident about the presence of a CNV; this is especially true for obvious large calls; for short exons: BF are bound to be unconvincing
             output.cnv.file = paste(sample,"_CNVs_ExomeDepth_",prefix,".txt",sep='')
             cat("\nOutput file:",output.cnv.file ,"\n")
-            write.table(all_exons@CNV.calls[order ( all_exons@CNV.calls$BF, decreasing = TRUE),], file=output.cnv.file, 
+            write.table(all_exons@CNV.calls[order ( all_exons@CNV.calls$BF, decreasing = TRUE),], file=output.cnv.file,
                 sep='\t', row.names=FALSE, col.names=TRUE, quote=FALSE)
         }
         else {#no CNVs found
